f2e67a01c429050b4420a92356505049898c37b3
lowchiakey-arch <lowchiakey@gmail.com>
2026-02-04 14:08:46 +0800

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add
+
+def test_add_positive_numbers():
+    result = add(5, 7)
+    assert result == 12
+
+def test_add_negative_numbers():
+    result = add(-1, -3)
+    assert result == -4
+
+def test_add_zero():
+    result = add(10, 0)
+    assert result == 10
